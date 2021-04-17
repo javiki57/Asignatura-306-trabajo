7146d1d9cfd61bcbd5a82334f6d9d650ed051f41
Javiki <javipatinador@gmail.com>
2021-04-17 20:46:17 +0200

    Dependencias e interfaz
    Se han añadido las dependencias necesarias para el
     proyecto EJB, he creado el paquete y la interfaz para
     los métodos de matrícula.

---
+++ jpa.proyect/src/main/java/modelo/er/jpa/ejb/GestionMatricula.java
@@ -0,0 +1,12 @@
+package modelo.er.jpa.ejb;
+
+import modelo.er.jpa.proyect.*;
+
+public interface GestionMatricula {
+
+	public void aniadirAsignatura(Asignatura a);
+	
+	public void eliminarAsignatura(Asignatura a);
+	
+	
+}
