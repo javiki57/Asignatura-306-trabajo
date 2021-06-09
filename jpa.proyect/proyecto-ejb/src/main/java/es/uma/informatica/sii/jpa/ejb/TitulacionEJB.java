068f173d07a7be1e445c6f5d9ec08654cc920d8c
psychys <swaglord@uma.es>
2021-06-09 07:58:18 -0700

    Añadidos metodos en titulacionRJB

--- jpa.proyect/proyecto-ejb/src/main/java/es/uma/informatica/sii/jpa/ejb/TitulacionEJB.java
+++ jpa.proyect/proyecto-ejb/src/main/java/es/uma/informatica/sii/jpa/ejb/TitulacionEJB.java
@@ -1,9 +1,13 @@
 package es.uma.informatica.sii.jpa.ejb;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import es.uma.informatica.sii.jpa.exceptions.TitulacionNoEncontradaException;
+import es.uma.informatica.sii.jpa.proyect.Centro;
 import es.uma.informatica.sii.jpa.proyect.Titulacion;
 
 @Stateless
@@ -19,6 +23,22 @@ public class TitulacionEJB implements GestionTitulacion{
 	
 	public void actualizarTitulacion(Titulacion t) {
 		em.merge(t);
+	}
+	
+	public Titulacion buscarTitulacion(String nombre) throws TitulacionNoEncontradaException {
+		
+		Titulacion tit = em.find(Titulacion.class, nombre);
+		
+		if(tit == null)
+			throw new TitulacionNoEncontradaException();
+		
+		return tit;
+	}
+	
+	public List<Titulacion> mostrarTitulaciones(Centro centro){
+		int centroID = centro.getId();
+		return (List<Titulacion>) em.createQuery("SELECT TITULACION FROM CENTRO WHERE id = centroID");
+		
 	}
 	
 	
